8afffc0af3e358b693467dec20cb184729478e83
Rob Benton <rob.benton@mainstream-tech.com>
2025-12-12 10:37:18 -0600

    setup refs

--- apps/sample-ui/src/pages/_app.tsx
+++ apps/sample-ui/src/pages/_app.tsx
@@ -1,18 +1,22 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { MainMenu } from '@nx-sample/components';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+export default function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Welcome to apps/sample-ui!</title>
       </Head>
-      <main className="app">
+      <main>
+        <MainMenu />
         <Component {...pageProps} />
       </main>
     </>
   );
 }
-
-export default CustomApp;
---
+++ libs/components/src/lib/main-menu/main-menu.tsx
@@ -0,0 +1,7 @@
+import { AppBar, Typography } from '@mui/material';
+
+export const MainMenu: React.FC = () => (
+  <AppBar>
+    <Typography>Main Menu</Typography>
+  </AppBar>
+);
