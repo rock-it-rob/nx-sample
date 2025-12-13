6f3bf39b7a651f8025d01c116a991f492401a5f3
Rob Benton <rob.benton7@gmail.com>
2025-12-13 15:28:11 -0600

    Works but dependencies needed
    And rules for banning imports.

--- libs/components/src/lib/main-menu/main-menu.tsx
+++ libs/components/src/lib/main-menu/main-menu.tsx
@@ -1,7 +1,19 @@
-import { AppBar, Typography } from '@mui/material';
+import { AppBar, Grid, Typography } from '@mui/material';
+import { dayjs } from '@nx-sample/dates';
 
-export const MainMenu: React.FC = () => (
-  <AppBar>
-    <Typography>Main Menu</Typography>
-  </AppBar>
-);
+export const MainMenu: React.FC = () => {
+  const now = dayjs().format('MM/DD/yyyy HH:mm:ss');
+
+  return (
+    <AppBar>
+      <Grid container={true}>
+        <Grid size={12}>
+          <Typography>Main Menu</Typography>
+        </Grid>
+        <Grid size={12}>
+          <Typography>Now: {now}</Typography>
+        </Grid>
+      </Grid>
+    </AppBar>
+  );
+};
--- libs/dates/src/lib/custom-dayjs.ts
+++ libs/dates/src/lib/custom-dayjs.ts
@@ -1,10 +1,8 @@
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(timezone);
 dayjs.extend(utc);
 
-export function customDayjs(): string {
-  return 'dates';
-}
+export { dayjs, Dayjs };
