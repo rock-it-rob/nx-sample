01f2e56746bd3a0be7837922c9af4661d53931a8
Rob Benton <rob.benton@mainstream-tech.com>
2026-01-16 13:13:02 -0600

    works with next

--- libs/components/src/lib/main-menu/main-menu.tsx
+++ libs/components/src/lib/main-menu/main-menu.tsx
@@ -1,10 +1,13 @@
 import { AppBar, Box, Typography } from '@mui/material';
+import Image from 'next/image';
 import { dayjs } from '@nx-sample/dates';
+import bars from '@nx-sample/components/assets/bars.svg';
 
 export const MainMenu = () => {
   return (
     <AppBar sx={{ p: '10px' }}>
       <Box display={'flex'} justifyContent={'space-around'}>
+        <Image src={bars} alt={'bars'} />
         <Typography>Main Menu</Typography>
         <Typography suppressHydrationWarning={true}>
           {dayjs().format('MM/DD/YYYY HH:mm:ss')}
